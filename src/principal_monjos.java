da0654ca7f79eb68a24d536064245c63f99fb2ea
MFloresr <mrsitofull@gmail.com>
2014-11-06 16:57:46 +0100

    gg

--- src/principal_monjos.java
+++ src/principal_monjos.java
@@ -18,6 +18,8 @@ public final class principal_monjos {
 		String nombre;
 		int dinero;
 		int piedra;
+		int sumaDinero = 0;
+		int cantidadDeBilletes = 0;
 		//INGRESAMOS 5 MONJES
 		for (int i =0 ; i<5; i++){
 
@@ -25,18 +27,40 @@ public final class principal_monjos {
 			dinero = sujeto.DarDinero();
 			piedra = sujeto.DarPiedras();
 			monjos.add(sujeto);
+			//monjos.add(sujeto.DarNombre(),sujeto.DarDinero(),sujeto.DarPiedras());
 			System.out.println(nombre+" "+ dinero+ " " + piedra);
 		}
 
 		//PNTAMOS PRECIO BILLETE
 		System.out.println("PRECIO DEL BILLETE : " + billet);
 
-		//TIRAMOS DADO y PINTAMOS VALOR
-		numero.TirarDado();
-		System.out.println("NUMERO DE LA SUERTE : " + numero.getNumero());
-
 		for(monjo o: monjos){
-			System.out.println(o);
+			System.out.println(o.getNombre()+" "+o.getDinero()+" "+o.getPiedras());
+		}
+
+		//TOTAL DINERO
+		for (int i = 0;i<monjos.size();i++){
+			sumaDinero += sujeto.getDinero();
+		}
+		System.out.println(sumaDinero);
+
+		//CANTIDAD DE BILLETES
+		while (!(sumaDinero < billet)){
+			sumaDinero = sumaDinero-billet;
+			cantidadDeBilletes ++;
+		}
+		System.out.println(cantidadDeBilletes);
+
+		//PRINCIPAL
+		while ( monjos.size() > cantidadDeBilletes){
+			numero.TirarDado();
+			System.out.println("NUMERO DE LA SUERTE : " + numero.getNumero());
+			for (int i = 0; i < monjos.size(); i++){
+				if (numero.getNumero() == monjos.get(i).getPiedras()){
+					System.out.println(monjos.get(i).getNombre()+" "+monjos.get(i).getDinero()+" "+monjos.get(i).getPiedras());
+					monjos.remove(i);
+				}
+			}
 		}
 
 		/*for (int i =0 ; i<monjos.size(); i++){
